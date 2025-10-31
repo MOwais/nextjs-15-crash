d72f2e0f882ae57507cea8d980259644422e1069
MOwais <owace.ahmed@gmail.com>
2025-10-30 20:38:51 -0700

    metadata for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,13 @@
-"use client";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us | Name of Website",
+  description: "A lot of keywords for SEO",
+  keywords: "about, company, mission, ai, saas",
+  twitter: {
+    card: "summary_large_image",
+  },
+};
 
 export default function About() {
   return <div>About page</div>;
